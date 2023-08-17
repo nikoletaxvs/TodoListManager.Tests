b30d55ca348d5525d3c9615ea42680798122e498
nikoletaxvs <nikoletaxvs@gmail.com>
2023-08-18 00:28:59 +0300

    initial commit

---
+++ Controllers/ItemsControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoListManager.Controllers;
+using TodoListManager.Interfaces;
+using TodoListManager.Models;
+using TodoListManager.Repositories;
+
+namespace TodoListManager.Tests.Controllers
+{
+    public class ItemsControllerTests
+    {
+        readonly private IItemRepository _itemRepository;
+        public ItemsControllerTests()
+        {
+            _itemRepository = A.Fake<IItemRepository>();
+        }
+        [Fact]
+        public void ItemsControllerTests_ReturnOk()
+        {
+            //Arrange
+           
+        }
+    }
+}
---
+++ Controllers/TodosControllerTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoListManager.Controllers;
+using TodoListManager.Data;
+using TodoListManager.Interfaces;
+using TodoListManager.Models;
+using Xunit;
+using FakeItEasy;
+using FluentAssertions;
+
+namespace TodoListManager.Tests.Controllers
+{
+	public class TodosControllerTests{
+        private readonly ITodoRepository _todoRepository;
+        public TodosControllerTests()
+		{
+			_todoRepository = A.Fake<ITodoRepository>();
+		}
+		[Fact]
+		public void TodosController_GetTodos_ReturnOk()
+		{
+			//Arrange
+			var todos = A.Fake<ICollection<Todo>>();
+			var controller = new TodosController(_todoRepository);
+
+			//Act
+			var result = controller.GetTodos();
+
+			//Assert
+			result.Should().NotBeNull();
+			result.Should().BeOfType(typeof(OkObjectResult));
+		}
+
+		
+		[Fact]
+		public void TodosController_CreateTodo_ReturnOk()
+		{
+			//Arrange 
+			var todo = A.Fake<Todo>();
+			var controller = new TodosController(_todoRepository);
+
+			//Act
+			var result = controller.CreateTodo(todo);
+
+			//Assert
+			result.Should().NotBeNull();
+		}
+
+        [Fact]
+        public void TodosController_GetTodo_ReturnOK()
+        {
+            //Arrange 
+            var todo = A.Fake<Todo>();
+            var controller = new TodosController(_todoRepository);
+
+            //Act
+            var result = controller.GetTodo(todo.Id);
+
+			//Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType(typeof(OkObjectResult));
+        }
+
+
+    }
+}
